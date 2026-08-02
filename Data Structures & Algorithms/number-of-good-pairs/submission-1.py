3059673d499c1cbdb6f5ce0ce3563b7227c6fe4a
Dewansh <dewanshwadhwa@gmail.com>
2026-08-02 15:59:25 +0530

    Add: number-of-good-pairs - submission-1

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-1.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        freq = defaultdict(int)
+        ans = 0
+
+        for num in nums:
+            ans += freq[num]
+            freq[num]+=1
+        return ans
